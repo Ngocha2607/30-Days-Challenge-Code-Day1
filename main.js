afcfc555c870d05bfe0999b48caeac4317347ef8
Lê Ngọc Hà <58633426+Ngocha2607@users.noreply.github.com>
2021-12-02 21:43:25 +0700

    Model-Popup

---
+++ main.js
@@ -0,0 +1,17 @@
+var btnOpen= document.querySelector('.open-model-btn');
+var model= document.querySelector('.model');
+var iconClose= document.querySelector('.model-header i');
+var btnClose= document.querySelector('.model-footer button');
+
+function toggleModel() {
+    model.classList.toggle('hide');
+}
+
+btnOpen.addEventListener('click', toggleModel);
+iconClose.addEventListener('click', toggleModel);
+btnClose.addEventListener('click', toggleModel);
+model.addEventListener('click', function(e) {
+    if(e.target== e.currentTarget) {
+        toggleModel();
+    }
+});
